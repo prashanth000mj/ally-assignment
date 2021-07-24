606c635947ba6fc3d482962100f3849a200bafbd
Prashanth MJ <prashanth@goexfx.com>
2021-07-24 12:59:42 +0530

    updated CRA defaults for assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import OKRs from './OKRs';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ally.io Assignment</h1>
       </header>
+      <OKRs />
     </div>
   );
 }
---
+++ src/OKRs.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const OKRs = () => {
+  return (<section>
+    <h2><u>Objectives & Key Results</u></h2>
+  </section>);
+}
+
+export default OKRs;
